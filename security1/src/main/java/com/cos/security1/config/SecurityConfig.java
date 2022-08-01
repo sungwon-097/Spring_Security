ec13657b186b191fc05c34582e99b2ddb30f1ad4
sungwon-097 <csw2757@gmail.com>
2022-08-01 17:02:42 +0900

    SpringSecurity : google profile

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // Spring Security Filter 가 Spring Filter Chain 에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // @Secured, @PreAuthorize, @PostAuthorize 활성화
 public class SecurityConfig { // WebSecurityConfigureAdapter 가 Deprecated. @Bean 등록으로 SecurityConfig 구현 가능
+
+    @Autowired
+    private PrincipalOauthUserService principalOauthUserService;
 
     @Bean // 해당 method 의 return object 를 IoC 로 등록해준다
     public BCryptPasswordEncoder encodePwd(){
@@ -30,7 +35,14 @@ public class SecurityConfig { // WebSecurityConfigureAdapter 가 Deprecated. @Be
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 이라는 주소가 호출되면 security 가 낚아채서 대신 로그인을 진행해줌 -> controller 에 /login 을 만들지 않아도
-                .defaultSuccessUrl("/"); // 특정페이지를 요청한 후에 로그인을 했다면 그 페이지로 보낸다
+                .defaultSuccessUrl("/") // 특정페이지를 요청한 후에 로그인을 했다면 그 페이지로 보낸다
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인이 완료 된 후의 후처리가 필요함
+                .userInfoEndpoint()
+                .userService(principalOauthUserService);
+        // 1. Code 2. AccessToken(permit) 3. user information
+        // 4-1. (Extra information) 4-2. sign up
         return http.build();
     }
 }
