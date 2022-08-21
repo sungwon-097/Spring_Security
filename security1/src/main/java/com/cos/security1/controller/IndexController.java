811dfb4e960ea8f73a9529020c96fe16af1a48a9
sungwon-097 <csw2757@gmail.com>
2022-08-21 19:45:16 +0900

    modified : controller

--- security1/src/main/java/com/cos/security1/controller/IndexController.java
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -24,7 +24,7 @@ public class IndexController {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @GetMapping({"", "./"})
+    @GetMapping("/")
     public String index(){
         return "index";
     }
